2e300c3c374de0f4446165bce939751f79cc76a3
Belavel77 <134178105+wiquipo@users.noreply.github.com>
2023-09-04 11:38:47 -0300

    Add files via upload

---
+++ myworld/myapp/templates/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings: ['Frontend Desarrollador', 'Estudiante',
+    'Blogger'],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+});
